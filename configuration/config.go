bdaaca46213b5f1eddcc6be3bf8b09152473821e
Deepanjal Kumar <55708909+Deepanjalkumar@users.noreply.github.com>
2021-06-28 19:26:15 +0530

    Add files via upload

---
+++ configuration/config.go
@@ -0,0 +1,13 @@
+package configuration
+
+var Binary_api string = ""
+
+var Chaos_api string = ""
+
+var Recondev_api string = ""
+
+var Security_trails_api string = ""
+
+var Shodan_api string = ""
+
+var Virus_total_api string = ""
